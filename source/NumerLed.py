2a825f9abf9f21896eb2fa8b5c631d072bf5dc90
Hermes Espinola <hermesespinola@gmail.com>
2016-03-14 22:08:22 +0000

    Update NumberLed.py

--- source/NumerLed.py
+++ source/NumerLed.py
@@ -1,30 +1,22 @@
 import time
 import pyupm_grove as grove
-import pyupm_buzzer as upmBuzzer
 import pyupm_i2clcd as lcd
 
 # Create the button object using GPIO pin 0
-button = grove.GroveButton(4)
-buzzer = upmBuzzer.Buzzer(8)
+button = grove.GroveButton(8)
 myLcd = lcd.Jhd1313m1(4, 0x3E, 0x62)
-
+valor = 223
 myLcd.setCursor(0,0)
-# RGB Blue
-#myLcd.setColor(53, 39, 249)
+myLcd.setColor(53, 39, 249)
+myLcd.write(str(valor))
 
-# RGB Red
-myLcd.setColor(255, 0, 0)
-
-myLcd.write('Hello World')
-
-
-# Read the input and print, waiting one second between readings
-while 1:
+while True:
           if (button.value() != 0):
-                    buzzer.playSound(upmBuzzer.DO, 1000000)
-          time.sleep(1)
+		 valor -=1
+		 myLcd.setCursor(0,0)
+		 myLcd.write(str(valor))
+          time.sleep(0.1)
 
-# Delete the button object
 del button
 # Delete the buzzer object
-del buzzer
+# del buzzer
