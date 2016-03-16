f75c406f22ae33d06d46760bf8bda0ddae1ac997
nerii94 <nerii_94@hotmail.com>
2016-03-16 12:32:10 -0600

    update color

--- source/ParkingV4-color.py
+++ source/ParkingV4-color.py
@@ -65,14 +65,15 @@ def enter(button, tId, url):
     if(button.value() != 0):
         if (valor > 0):
             valor -= 1
-            if ((capacity/valor) <= 0.25):
-                myLcd.setColor(0, 255, 0)
-            elif((capacity/valor) <= 0.50):
-                myLcd.setColor(255, 255, 0)
-            elif((capacity/valor) <= 0.75):
-                myLcd.setColor(255, 165, 0)
-            elif((capacity/valor) <= 1):
-                myLcd.setColor(255, 0, 0)
+            
+            if ((valor / capacity) <= 0.25):
+		myLcd.setColor(0,255,0)
+	    elif((valor / capacity) <= 0.50):
+		myLcd.setColor(255,255,0)
+	    elif((valor / capacity) <= 0.75):
+		myLcd.setColor(255,165,0)
+	    elif((valor / capacity) <= 1):
+		myLcd.setColor(255,0,0)
             myLcd.setCursor(1,11)
             myLcd.write(str(valor))
             r = requests.put(url+"/"+tId)
