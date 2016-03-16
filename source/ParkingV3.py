ad3f6fe4f7241c68f0eb979e54a7b0b86f72f8ec
nerii94 <nerii_94@hotmail.com>
2016-03-16 11:08:19 -0600

    create ParkingV3.py

---
+++ source/ParkingV3.py
@@ -0,0 +1,59 @@
+import time
+import pyupm_ttp223 as ttp223
+import requests
+import json
+import pyupm_grove as grove
+import pyupm_i2clcd as lcd
+
+urlEntrada = "http://secure-badlands-53433.herokuapp.com/enterzone"
+urlSalida = "http://secure-badlands-53433.herokuapp.com/leavezone"
+urlzone = "http://secure-badlands-53433.herokuapp.com/zones"
+
+def checkCapacity(tId,url):
+    global valor
+    r = requests.get(url+"/"+tId)
+    data = r.json()
+    return data["capacity"] - data["full"]
+
+def checkZone(tId,url):
+    r = requests.get(url+"/"+tId)
+    data = r.json()
+    return data["name"]
+    
+touch1 = ttp223.TTP223(4)
+
+# Create the button object using GPIO pin 0
+button = grove.GroveButton(8)
+myLcd = lcd.Jhd1313m1(4, 0x3E, 0x62)
+valor = checkCapacity("Z2", urlzone)
+
+myLcd.setCursor(0, 0)
+myLcd.write("zone: " + checkZone)
+myLcd.setColor(53, 39, 249)
+myLcd.setCursor(0, 1)
+myLcd.write("Available: " +str(valor))
+
+def exit(touch, tId, url):
+    global valor
+    if touch.isPressed():
+        valor += 1
+        myLcd.setCursor(12, 1)
+        myLcd.write(str(valor))
+        r = requests.put(url+"/"+tId)
+
+def enter(button, tId, url):
+    global valor
+    if(button.value() != 0):
+        valor -= 1
+        myLcd.setCursor(12, 1)
+        myLcd.write(str(valor))
+        r = requests.put(url+"/"+tId)
+        
+
+while True:
+    exit(touch1,"Z2", urlSalida)
+    enter(button,"Z2", urlEntrada)
+    time.sleep(0.2)
+
+del touch1
+del button
