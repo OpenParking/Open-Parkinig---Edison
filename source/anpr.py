4a4eaf458e7a9931c0766dc5f41031960f07508f
Hermes Espínola <hermesespinola@gmail.com>
2016-03-16 03:05:10 -0600

    anpr not working (yet)

--- source/anpr.py
+++ source/anpr.py
@@ -4,14 +4,36 @@ import sys
 import os
 import requests
 import json
-from PIL import Image
-from pytesseract import *
+from pytesseract import image_to_string
+from pytesseract import pytesseract as ocr
 
 vidCap = cv2.VideoCapture(0)
 
 while True:
-    ret, original = vidCap.read()
-    resize = cv.CreateMat(original.rows / 10, original.cols / 10, original.type)
-    cv.Resize(original, resize)
-    CvtColor(original, gray, CV_RGB2GRAY)
-    cvThreshold(image, binary_image, 128, 255, CV_THRESH_OTSU)
+    small = cv2.imread("plate.jpg")  # vidCap.read()
+    small = cv2.resize(small, (0, 0), fx=0.5, fy=0.5)
+    small = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
+    small = cv2.medianBlur(small, 5)
+
+    # Otsu's thresholding after Gaussian filtering
+    retOtsu, thOtsu = cv2.threshold(small, 128, 255, cv2.THRESH_OTSU)
+    
+    # temporately write images to storage
+    tempOtsu = 'plateTgOtsu.jpeg'
+    cv2.imwrite(tempOtsu, thOtsu)
+    tempOtsuTiff = 'plateTgOtsu.tiff'
+    img = ocr.Image.open(tempOtsu)
+    text = ocr.image_to_string(img)
+
+    # delete temp files
+    os.remove(tempOtsu)
+    # os.remove(tempOtsuTiff)
+
+    print text
+
+    # img = Image.open(original)
+    # img.save('plate.tiff')
+    # im = Image.open(img)
+    # text = image_to_string(im)
+    # text = image_file_to_string(image_file)
+    break
